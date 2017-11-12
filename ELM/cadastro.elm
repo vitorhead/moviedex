977e5a4be215dae82bc5eb2b085752b6cdc69bcb
Vitor Stipanich <vitorstipanich@hotmail.com>
2017-11-12 19:21:34 +0000

    [ELM] Módulo de cadastro - Tipos, init e url do GET

--- ELM/cadastro.elm
+++ ELM/cadastro.elm
@@ -22,6 +22,51 @@ httpErrorRetorno error =
 
         BadPayload message response -> ("[ERRO HTTP] Payload incorreto: "++ 
              toString message++"("++toString response.status.code++")") 
+
+type alias Retorno =
+    {
+     mensagem : Int --id do cadastro
+    ,codigo : Int
+    }
+
+type alias Cadastro =
+    { 
+     email        : String
+    ,nome         : String
+    ,dtNascimento : String
+    ,senha        : String
+    ,sexo         : String
+    }
+    
+type alias Model =
+    {
+     nome         : String
+    ,dtNascimento : String
+    ,sexo         : String
+    ,email        : String
+    ,senha        : String
+    ,retorno      : Retorno
+    ,error        : String
+    }
+
+type Message = 
+      Nome String
+    | DtNascimento String
+    | Sexo String
+    | Email String
+    | Senha String
+    | Submit
+    | Response (Result Http.Error Retorno)
+
+init : Model
+init = 
+    let
+        retornoIni = Retorno 0 0
+    in
+        (Model "" "" "" "" "" retornoIni "")
+
+urlPOST : String
+urlPOST = "https://haskelleta-romefeller.c9users.io/cadastro/inserir" 
              
 main =
   program 
