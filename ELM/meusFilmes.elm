fa808538869334face956de2f746e6591b442a91
Yohann Castilho <yohann.castilho94@gmail.com>
2017-12-02 15:37:12 -0200

    [ELM] css da pagina logada

--- ELM/meusFilmes.elm
+++ ELM/meusFilmes.elm
@@ -9,17 +9,17 @@ import BuscaFilme as BF exposing(..)
 
 type alias Model =
     {
-     meusFilmes : MeusFilmes    
+     meusFilmes : MeusFilmes
     }
 
-type alias MeusFilmes = 
+type alias MeusFilmes =
     {
         resp : List (BF.FilmeResult)
     }
-    
+
 init : Model
 init = Model <| []
-    
+
 
 decodeMeusFilmes : Decoder MeusFilmes
 decodeMeusFilmes = Json.Decode.map MeusFilmes (at ["resp"] (Json.Decode.list BF.decodeFilmeResult))
@@ -33,7 +33,51 @@ getMeusFilmes idcad =
 
 view : Model -> Html Message
 view model =
-    div [] []
+  div [class "row"]
+  [
+    div [class "sidebar col s12 m4 l3"]
+    [
+      div [class "lateral-principal"]
+      [
+        ul []
+        [
+          li [] [text "NOME CHAMPS"]
+          ,li []
+          [
+            a [class "btn green"] [text "Buscar Filmes"]
+          ]
+          ,li []
+          [
+            a [class "btn red"] [text "Deslogar"]
+          ]
+        ]
+      ]
+    ]
+    ,div [class "col s12 m8 l9"]
+    [
+      section []
+      [
+        h1 [] [text "NOME LISTA"]
+        ,ul [class "lista"]
+        [
+          li []  -- CRIAR UM LI NESSE ESTILO PARA CADA FILME
+          [
+            div [class "poster-filme"]
+            [
+              img [onClick ] [] --Mudar Pg-DETALHES
+            ]
+          ]
+          ,li []  -- CRIAR UM LI NESSE ESTILO PARA CADA FILME
+          [
+            div [class "poster-filme"]
+            [
+              img [onClick ] [] --Mudar Pg-DETALHES
+            ]
+          ]
+        ] -- fim ul
+      ] -- fim section
+    ]
+  ]
 
 
 main =
